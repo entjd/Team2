d0c578c2a9a48eb4a5d28737053216ea93f96fcb
wken5577 <wken5577@hanmail.net>
2021-12-14 00:25:41 +0900

    ci

--- src/first/HelloWorld2.java
+++ src/first/HelloWorld2.java
@@ -3,6 +3,8 @@ package first;
 public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("geee");
+		System.out.println("hello");
+		System.out.println("ddd");
+		
 	}
 }
